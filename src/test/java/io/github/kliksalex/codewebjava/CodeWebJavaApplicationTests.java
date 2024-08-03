23932829c7f84fe392735548ec5d2c38a9b8eecf
2kliksAlex <4kliksAlex@YouTube>
2024-08-03 17:15:56 +0800

    Initial

---
+++ src/test/java/io/github/kliksalex/codewebjava/CodeWebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kliksalex.codewebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWebJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
