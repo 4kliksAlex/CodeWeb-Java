88ba4ed8da7634051b3397bd966bfa8cc54911bb
2kliksAlex <4kliksAlex@YouTube>
2024-08-07 15:30:32 +0800

    wtf why `scene` folder comes back again?

--- src/main/java/io/github/kliksalex/codeweb/operationlayer/scene/ProjectSceneRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.kliksalex.codeweb.operationlayer.scene;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectSceneRepository extends JpaRepository<ProjectScene, Integer> {
-    // Define custom query methods if needed
-}
